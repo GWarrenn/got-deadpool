ee663d1ca62771cf303e0efe0d5b64c299f29bca
GWarrenn <augustjwarren@gmail.com>
2019-05-15 00:02:18 -0400

    Adding Battle of Kings Landing!

--- scripts/got_results.js
+++ scripts/got_results.js
@@ -1,165 +1,202 @@
-d3.csv("https://raw.githubusercontent.com/GWarrenn/got-deadpool/master/data/predictions.csv", function(data){
 
-	var updateTable = function(data,filter_param) {
+var battleSelector = d3.select('#battle')
 
-		document.getElementById("name").innerHTML = "Deadpool Predictions for: <b>" + filter_param + "</b>"
+battleSelector
+  .selectAll("option")
+  .data(['Winterfell','Kings Landing'])
+  .enter()
+  .append("option")
+	.attr("value", function (d) { return d; })
+	.text(function (d) {
+		return d[0].toUpperCase() + d.slice(1,d.length);
+	})
 
-		d3.select("#character-predictions-table tbody").remove();
-		d3.select("#character-predictions-table thead").remove();
-
-		var table = d3.select('#character-predictions-table')
-			.append('table')
-
-		var thead = table.append('thead')
-		var	tbody = table.append('tbody');
-
-		data = _.orderBy(data, ['variable'], ['asc']);
-
-		display_cols = ['Character','Prediction','Actual Fate','Ruling']
-		columns = ['variable','value','actual','outcome']
-
-		filtered_data = data.filter(function (a) { return a.sup_email == filter_param ; });		
-
-		//// append the header row
-		thead.append('tr')
-		  .selectAll('th')
-		  .data(display_cols).enter()
-		  .append('th')
-			.text(function (column) { return column; });
-
-		// create a row for each object in the data
-		var rows = tbody.selectAll('tr')
-		  .data(filtered_data)
-		  .enter()
-		  .append('tr');
-
-		rows.exit().remove();
-
-		color = d3.scaleOrdinal()
-		    .domain(["Incorrect","Correct"])
-		    .range(["#ff9197","#90f98e"]);
-
-		// create a cell in each row for each column
-		cells = rows.selectAll('td')
-			.data(function (row) {
-				return columns.map(function (column) {
-					return {column: column, value: row[column]};
-				});
-			})
-			.enter()
-			.append('td')
-			.style("background-color", function(d){ if(d.column == "outcome") return color(d.value);})
-			.text(function (d) { return d.value; });
-
-		cells.exit().remove();
-
-	}	
-
-	temp_filter = 'alexa~payne7015'
-
-	updateTable(data,temp_filter)
-
-	list = _.uniqBy(data, function (e) {
-		return e.sup_email;
-	});
-
-	var names = _.map(list, 'sup_email').sort();
-
-	var dropDown = d3.select('#nameDropdown')
-
-	dropDown
-	  .selectAll("option")
-	  .data(names)
-	  .enter()
-	  .append("option")
-		.attr("value", function (d) { return d; })
-		.text(function (d) {
-			return d[0].toUpperCase() + d.slice(1,d.length);
-		})
-	  .on("change", onchange)
-
-	dropDown.on('change',function() {
+	battleSelector.on('change',function() {
 
 		var selectValue = d3.select(this)
 			.property('value');
 
-		updateTable(data,selectValue);
+		displayBattleResults(selectValue);
 
 	})
 
-});		
+displayBattleResults('Kings Landing')
 
-d3.csv("https://raw.githubusercontent.com/GWarrenn/got-deadpool/master/data/outcome_table.csv", function(data){
+function displayBattleResults(value){
 
-		document.getElementById("header").innerHTML = "Deadpool Rankings"
+	if(value == "Winterfell"){
+		file_1 = "https://raw.githubusercontent.com/GWarrenn/got-deadpool/master/data/predictions.csv"
+		file_2 = "https://raw.githubusercontent.com/GWarrenn/got-deadpool/master/data/outcome_table.csv"
+	} 
 
-		var format = d3.format(",.2%")
+	else if(value == "Kings Landing"){
+		file_1 = "https://raw.githubusercontent.com/GWarrenn/got-deadpool/master/data/predictions_westeros.csv"
+		file_2 = "https://raw.githubusercontent.com/GWarrenn/got-deadpool/master/data/outcome_table_westoros.csv"
+	} 
 
-		// format the data
-		data.forEach(function(d) {
-			d.rank = +d.rank
-			d.Correct = +d.Correct
-			d.Correct_fmt = format(+d.Correct)
-			//d.Correct_fmt = +d.Correct_fmt
+	d3.csv(file_1, function(data){
+
+		var updateTable = function(data,filter_param) {
+
+			document.getElementById("name").innerHTML = "Deadpool Predictions for: <b>" + filter_param + "</b>"
+
+			d3.select("#character-predictions-table tbody").remove();
+			d3.select("#character-predictions-table thead").remove();
+
+			var table = d3.select('#character-predictions-table')
+				.append('table')
+
+			var thead = table.append('thead')
+			var	tbody = table.append('tbody');
+
+			data = _.orderBy(data, ['variable'], ['asc']);
+
+			display_cols = ['Character','Prediction','Actual Fate','Ruling']
+			columns = ['variable','value','actual','outcome']
+
+			filtered_data = data.filter(function (a) { return a.sup_email == filter_param ; });		
+
+			//// append the header row
+			thead.append('tr')
+			  .selectAll('th')
+			  .data(display_cols).enter()
+			  .append('th')
+				.text(function (column) { return column; });
+
+			// create a row for each object in the data
+			var rows = tbody.selectAll('tr')
+			  .data(filtered_data)
+			  .enter()
+			  .append('tr');
+
+			rows.exit().remove();
+
+			color = d3.scaleOrdinal()
+			    .domain(["Incorrect","Correct"])
+			    .range(["#ff9197","#90f98e"]);
+
+			// create a cell in each row for each column
+			cells = rows.selectAll('td')
+				.data(function (row) {
+					return columns.map(function (column) {
+						return {column: column, value: row[column]};
+					});
+				})
+				.enter()
+				.append('td')
+				.style("background-color", function(d){ if(d.column == "outcome") return color(d.value);})
+				.text(function (d) { return d.value; });
+
+			cells.exit().remove();
+
+		}	
+
+		temp_filter = 'alexa~payne7015'
+
+		updateTable(data,temp_filter)
+
+		var list = _.uniqBy(data, function (e) {
+			return e.sup_email;
 		});
 
-		d3.select("#outcomes-table tbody").remove();
-		d3.select("#outcomes-table thead").remove();
+		var names = _.map(list, 'sup_email').sort();
 
-		var table = d3.select('#outcomes-table')
-			.append('table')
+		var dropDown = d3.select('#nameDropdown')
 
-		var thead = table.append('thead')
-		var	tbody = table.append('tbody');
-
-		data = _.orderBy(data, ['rank'], ['asc']);
-
-		display_cols = ['Name','Percent Correct','Rank']
-		columns = ['sup_email','Correct_fmt','rank']
-
-		//// append the header row
-		thead.append('tr')
-		  .selectAll('th')
-		  .data(display_cols).enter()
-		  .append('th')
-			.text(function (column) { return column; });
-
-		// create a row for each object in the data
-		var rows = tbody.selectAll('tr')
-		  .data(data)
+		dropDown
+		  .selectAll("option")
+		  .data(names)
 		  .enter()
-		  .append('tr');
-
-		rows.exit().remove();
-
-		min = _.minBy(data, function(o) {
-				return o.Correct_fmt;
-		})
-
-		max = _.maxBy(data, function(o) {
-				return o.Correct_fmt;
-		})
-
-		color = d3.scaleLinear()
-		    .domain([90,45])
-		    .range(["#ff4500","#ffffff"]);
-
-		back_to_number = d3.format(".4r")
-
-		// create a cell in each row for each column
-		cells = rows.selectAll('td')
-			.data(function (row) {
-				return columns.map(function (column) {
-					return {column: column, value: row[column]};
-				});
+		  .append("option")
+			.attr("value", function (d) { return d; })
+			.text(function (d) {
+				return d[0].toUpperCase() + d.slice(1,d.length);
 			})
-			.enter()
-			.append('td')
-			.style("background-color", function(d){ if(d.column == "Correct_fmt") return color(+d.value.replace("%",""));})
-			.text(function (d) { return d.value; });
+		  .on("change", onchange)
 
-		cells.exit().remove();	
+		dropDown.on('change',function() {
 
-});		
+			var selectValue = d3.select(this)
+				.property('value');
+
+			updateTable(data,selectValue);
+
+		})
+
+	});		
+
+	d3.csv(file_2, function(data){
+
+			document.getElementById("header").innerHTML = "Deadpool Rankings"
+
+			var format = d3.format(",.2%")
+
+			// format the data
+			data.forEach(function(d) {
+				d.rank = +d.rank
+				d.Correct = +d.Correct
+				d.Correct_fmt = format(+d.Correct)
+				//d.Correct_fmt = +d.Correct_fmt
+			});
+
+			d3.select("#outcomes-table tbody").remove();
+			d3.select("#outcomes-table thead").remove();
+
+			var table = d3.select('#outcomes-table')
+				.append('table')
+
+			var thead = table.append('thead')
+			var	tbody = table.append('tbody');
+
+			data = _.orderBy(data, ['rank'], ['asc']);
+
+			display_cols = ['Name','Percent Correct','Rank']
+			columns = ['sup_email','Correct_fmt','rank']
+
+			//// append the header row
+			thead.append('tr')
+			  .selectAll('th')
+			  .data(display_cols).enter()
+			  .append('th')
+				.text(function (column) { return column; });
+
+			// create a row for each object in the data
+			var rows = tbody.selectAll('tr')
+			  .data(data)
+			  .enter()
+			  .append('tr');
+
+			rows.exit().remove();
+
+			min = _.minBy(data, function(o) {
+					return o.Correct_fmt;
+			})
+
+			max = _.maxBy(data, function(o) {
+					return o.Correct_fmt;
+			})
+
+			color = d3.scaleLinear()
+			    .domain([90,45])
+			    .range(["#ff4500","#ffffff"]);
+
+			back_to_number = d3.format(".4r")
+
+			// create a cell in each row for each column
+			cells = rows.selectAll('td')
+				.data(function (row) {
+					return columns.map(function (column) {
+						return {column: column, value: row[column]};
+					});
+				})
+				.enter()
+				.append('td')
+				.style("background-color", function(d){ if(d.column == "Correct_fmt") return color(+d.value.replace("%",""));})
+				.text(function (d) { return d.value; });
+
+			cells.exit().remove();	
+
+	});		
 
 
+}
